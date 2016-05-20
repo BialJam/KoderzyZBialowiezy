c786fa6a005f3c964c03b3c70b335f9266beb663
Pawel Kondraciuk <pawel.kondraciuk@codequest.com>
2016-05-20 20:17:13 +0200

    Music, background example

--- src/states/GameState.js
+++ src/states/GameState.js
@@ -2,9 +2,20 @@ import RainbowText from 'objects/RainbowText';
 
 class GameState extends Phaser.State {
 
+	preload() {
+		this.load.image('background', 'assets/background.jpg');
+		this.load.audio('music', 'assets/music.mp3');
+	}
+
 	create() {
+		this.background = this.game.add.sprite(0, 0, 'background');
+
+		let music = this.add.audio('music', 0.5, true);
+
+		music.play();
+
 		let center = { x: this.game.world.centerX, y: this.game.world.centerY }
-		let text = new RainbowText(this.game, center.x, center.y, "- phaser -\nwith a sprinkle of\nES6 dust!");
+		let text = new RainbowText(this.game, center.x, center.y, "- Koderzy z Białowieży -\nkontratakujo!");
 		text.anchor.set(0.5);
 	}
 
