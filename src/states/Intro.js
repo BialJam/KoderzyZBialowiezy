16f21e99471e8871c845185d5af00d41b0ee3ea3
dm <malyszodawid@gmail.com>
2016-05-20 23:26:29 +0200

    animation

--- src/states/Intro.js
+++ src/states/Intro.js
@@ -7,18 +7,24 @@ class Intro extends Phaser.State {
 	}
 
 	create() {
+		// music
 		let music = this.add.audio('music', 0.5, true);
 		music.play();
 
-		let logo = this.add.sprite(this.game.world.centerX, -200, 'logo');
-		logo.anchor.set(0.5);
-		this.physics.enable(logo, Phaser.Physics.ARCADE);
-		logo.body.velocity.y = 100;
-
-		let style = { font: "46px Press Start 2P", align: "center", fill: "white" };
-		let text = this.game.add.text(this.game.world.centerX, this.game.world.centerY, "Bede grau w gre", style)
+		// copyrights text
+		let style = { font: "26px Press Start 2P", align: "center", fill: "white" };
+		let text_value = 'M   AND   ©     2016 \n \
+				KODERZY    Z     BIALOWIEZY    CO    LTD \n \
+				LICENSED    BY\n NINTENDO    OF     AMERICA     INC'
+		let text = this.game.add.text(this.game.world.centerX, this.game.world.centerY + 200, text_value , style)
 		text.anchor.set(0.5);
 
+		// logo
+		let logo = this.add.sprite(this.game.world.centerX, -200, 'logo');
+		logo.anchor.set(0.5);
+		logo.enableUpdate = true;
+		let animation = this.add.tween(logo).to( { y: this.game.world.centerY-(logo.height/2) }, 5000, "Quart.easeOut");
+		animation.start();
 	}
 
 }
